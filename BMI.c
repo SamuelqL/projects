a4b614101579716f3746e314d7eaa5566acbe097
Samuel Quoc Le <samuelq.le@gmail.com>
2023-09-09 08:30:58 -0700

    bmi calculator

---
+++ BMI.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    float feet;
+    float inches;
+    printf("Enter your height(e.g 6'0): ");
+    scanf("%f'%f", &feet,&inches);
+
+    float weight;
+    printf("Enter your weight in pounds(e.g 150.4): ");
+    scanf("%f", &weight);
+
+    float TotalInches = (feet * 12) + inches;
+    float squared = TotalInches * TotalInches;
+
+    float BMI = weight/squared;
+
+    printf("Your BMI is %f", BMI);
+
+    if(BMI >= 30)
+    {
+        printf("\nYou are clinically obeset");
+    }
+    else
+    {
+        printf("\nYou are not obese!");
+    }
+
+
+    return 0;
+}
