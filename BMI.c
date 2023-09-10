7be1ad2ba8b6b16e5a195ac57cccce353bc50fd8
Samuel Quoc Le <samuelq.le@gmail.com>
2023-09-10 09:10:31 -0700

    Different systems for BMI

--- BMI.c
+++ BMI.c
@@ -1,21 +1,55 @@
 #include <stdio.h>
 #include<math.h>
+#include <ctype.h>
 
 int main()
 {
+
+    char system;
+    printf("Do you use the imperial(I) or the metric system?(M): ");
+    scanf(" %c", &system);
+    system = toupper(system);
+    
+
     float feet;
     float inches;
-    printf("Enter your height(e.g 6'0): ");
+    float weight;
+    float BMI;
+    float meters;
+    float centimeters;
+    float kilograms;
+    float squared;
+    if(system == 'I')
+    {
+        printf("Enter your height(e.g 6'0): ");
     scanf("%f'%f", &feet,&inches);
 
-    float weight;
     printf("Enter your weight in pounds(e.g 150.4): ");
     scanf("%f", &weight);
 
     float TotalInches = (feet * 12) + inches;
-    float squared = TotalInches * TotalInches;
+    squared = TotalInches * TotalInches;
 
-    float BMI = (weight/squared) * 703;
+    BMI = (weight/squared) * 703;
+    }
+    else if(system == 'M')
+    {
+        printf("Enter your height in centimeters(e.g 170.2): ");
+        scanf("%f", &centimeters);
+        printf("Enter your weight in kilos(e.g 85.7): ");
+        scanf("%f", &kilograms);
+
+        meters = centimeters/100;
+
+        squared = meters * meters;
+
+        BMI = kilograms/squared;
+    }
+    else
+    {
+        printf("That is not a valid system, enter again.");
+    }
+    
 
     printf("Your BMI is %.2f", BMI);
 
@@ -25,7 +59,7 @@ int main()
     }
     else
     {
-        printf("\nYou are not obese! STAY HARD!");
+        printf("\nYou are not obeset! STAY HARD!");
     }
 
 
