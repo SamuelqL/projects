7f253306468fab89fd452e8ceaa0619d4f860bf6
Samuel Quoc Le <samuelq.le@gmail.com>
2023-09-15 11:05:38 -0700

    enter user input into a file

--- pushuplog.c
+++ pushuplog.c
@@ -7,19 +7,31 @@ int main()
 {
     char buffer[BUFFER_SIZE];
     bool keep_going = true;
-    //FILE *pF;
-    //pF = fopen;
+    FILE *pF;
+    pF = fopen("C:\\Users\\samue\\OneDrive\\Desktop\\codingting.txt", "w");
 
-    while(keep_going)
+    if(pF == NULL)
+    {
+        printf("Error opening file.\n");
+        return 1;
+    }
+    printf("Enter how many pushups you did today: ");
+    fgets(buffer, BUFFER_SIZE, stdin);
+    fputs(buffer, pF);
+    fclose(pF);
+
+
+    /*while(keep_going)
     {
         fgets(buffer, BUFFER_SIZE, stdin);
-        printf("%s", buffer);
-
-        if (strcmp(buffer, "quit\n")== 0)
+        
+    if (strcmp(buffer, "quit\n")== 0)
     {
         keep_going = false;
     }
+    else fputs(buffer, pF);
     }
+    fclose(pF);*/
     
     return 0;
 
