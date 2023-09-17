a7d0d81ba7a39ab86c13288532044011cf97eaa8
Samuel Quoc Le <samuelq.le@gmail.com>
2023-09-17 08:39:18 -0700

    implemented current time and date into the log

--- pushuplog.c
+++ pushuplog.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 #define BUFFER_SIZE 1024
 
 int main()
 {
+    time_t t = time(NULL);
+    struct tm date = *localtime(&t);
+    FILE *ppF = fopen("C:\\Users\\samue\\OneDrive\\Desktop\\codingting.txt", "a");
+    fprintf(ppF, "%02d-%02d-%d|%02d:%02d\n", date.tm_mon + 1, date.tm_mday, date.tm_year + 1900, date.tm_hour, date.tm_min);
+    fclose(ppF);
+    
     char buffer[BUFFER_SIZE];
     bool keep_going = true;
     FILE *pF;
@@ -15,7 +23,7 @@ int main()
         printf("Error opening file.\n");
         return 1;
     }
-    printf("Enter how many pushups did you do today: ");
+    printf("\nEnter how many pushups did you do today: ");
     fgets(buffer, BUFFER_SIZE, stdin);
     fputs(buffer, pF);
     fclose(pF);
