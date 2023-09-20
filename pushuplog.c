b514c752da8386560d4d1a6c24558f6e2003f085
Samuel Quoc Le <samuelq.le@gmail.com>
2023-09-19 21:36:35 -0700

    coding log with some problems

--- pushuplog.c
+++ pushuplog.c
@@ -2,63 +2,105 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 #include <math.h>
 #define BUFFER_SIZE 1024
 
+bool hasEnteredAlready( const char *filename, const char *current_date){
+    FILE *fd;                           
+      
+   static const long max_len = 55+ 1;  
+   char buff[max_len + 1];            
 
+if ((fd = fopen(filename, "rb")) != NULL)  {      
 
+    fseek(fd, -max_len, SEEK_END);           
+    fread(buff, max_len-1, 1, fd);            
+    fclose(fd);                               
+
+    buff[max_len-1] = '\0';                   
+    char *last_newline = strrchr(buff, '\n'); 
+    char *last_line = last_newline+1; 
+    printf("%s\n", last_line);
+    
+    // split line by pipe
+    char *last_date;
+    
+    // get first element of token as date
+    last_date = strtok(last_line, "|"); 
+    printf("%s", last_date);
+
+    // compare last_date to curreent date
+    
+    if (strcmp(last_date, current_date)==0){
+        return true;
+    } else {
+        return false;
+    }
+    // char str_date[50] = ("%s", last_line);
+    // const char p[2] = "|";
+    
+    // while(token != NULL)
+    // {
+    //     printf("\n%s", token);
+        
+    //     token = strtok(NULL, p);
+    // }
+    
+    
+    // if same return true
+    // if false rretun false
+    
+}
+return false;
+}
 
 int main()
 {
     time_t t = time(NULL);
     struct tm date = *localtime(&t);
-    FILE *ppF;
+    const char filename[] = "C:\\Users\\samue\\OneDrive\\Desktop\\codingting.txt";
+    // 
+
+    char *current_date;
+    sprintf(current_date, "%02d-%02d-%d", date.tm_mon + 1, date.tm_mday, date.tm_year + 1900);
+    if (hasEnteredAlready(filename, current_date)){
+        printf("\nYou already logged");
+        return 0;
+    }
+    // printf("%s", current_date);
+
+    /*FILE *ppF = fopen(filename, "a");
+    fprintf(ppF, "%02d-%02d-%d|%02d:%02d, ", date.tm_mon + 1, date.tm_mday, date.tm_year + 1900, date.tm_hour, date.tm_min);
+    fclose(ppF);
+    char buffer[BUFFER_SIZE];
+    bool keep_going = true;
     FILE *pF;
+    pF = fopen(filename, "a");
 
-       
-            FILE *ppF = fopen("C:\\Users\\samue\\OneDrive\\Desktop\\codingting.txt", "a");
-            fprintf(ppF, "%02d-%02d-%d|%02d:%02d, ", date.tm_mon + 1, date.tm_mday, date.tm_year + 1900, date.tm_hour, date.tm_min);
-            fclose(ppF);
+    if(pF == NULL)
+    {
+       printf("Error opening file.\n");
+       return 1;
+    }               
+    printf("\nEnter how many pushups did you do today: ");
+    fgets(buffer, BUFFER_SIZE, stdin);
+    fputs(buffer, pF);
+    fclose(pF);
+
+    pF = fopen(filename, "r");
+
+    int current_line = 1;
+    char c;
+    do
+    {
+        c = fgetc(pF);
+        if(c == '\n') current_line++;
+    } while (c != EOF);
+
+    printf("%d", current_line - 1);*/
     
-            char buffer[BUFFER_SIZE];
-            bool keep_going = true;
-            FILE *pF;
-            pF = fopen("C:\\Users\\samue\\OneDrive\\Desktop\\codingting.txt", "a");
-
-                if(pF == NULL)
-                    {
-                        printf("Error opening file.\n");
-                        return 1;
-                    }               
-            printf("\nEnter how many pushups did you do today: ");
-            fgets(buffer, BUFFER_SIZE, stdin);
-            fputs(buffer, pF);
-            fclose(pF);
-
-            pF = fopen("C:\\Users\\samue\\OneDrive\\Desktop\\codingting.txt", "r");
-
-            int current_line = 1;
-            char c;
-            do
-                {
-                    c = fgetc(pF);
-                    if(c == '\n') current_line++;
-                } while (c != EOF);
-
-                printf("%d", current_line - 1);
-
-    
-
-    
-
-    
-    
-
-    
-
-
-
 
     /*while(keep_going)
     {
