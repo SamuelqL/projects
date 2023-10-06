2a2331b3693b1e625041c2008b3f3722a13bfef0
Samuel Quoc Le <samuelq.le@gmail.com>
2023-10-05 18:13:15 -0700

    getting last line but with issues

--- pushuplog.c
+++ pushuplog.c
@@ -7,7 +7,7 @@
 #include <math.h>
 #define BUFFER_SIZE 1024
 
-bool hasEnteredAlready( const char *filename, char *current_date){
+/*bool hasEnteredAlready( const char *filename, char *current_date){
     
     FILE *fd;                           
       
@@ -52,6 +52,24 @@ if ((fd = fopen(filename, "rb")) != NULL)  {
     
 }
 return false;
+}*/
+void get_line_num(char *filename){
+    FILE *pF;
+    pF = fopen(filename, "r");
+
+    int current_line = 0;
+    char c;
+    do
+    {
+        c = fgetc(pF);
+        if(c == '\n') current_line++;
+    } while (c != EOF);
+
+    //printf("%d", current_line);
+    fclose(pF);
+
+    
+
 }
 
 void log_pushup(char *filename){
@@ -149,56 +167,21 @@ int main()
     
     // get a line of log
     char *log_line = get_log_line(0, filename);
-
+    
     // extract out pushup number
     //char *s = ",";
     //char *pushups = get_line_element(log_line, 1, s);
-
     //printf("%s", pushups);
+
+    int current_line;
+    get_line_num(filename);
+    char *last_line = get_log_line(current_line, filename);
+
+    printf("\n%s", last_line);
+
     // get_pushup_number(log_line);
     // char *current_date;
     //sprintf(current_date, "%02d-%02d-%d", date.tm_mon + 1, date.tm_mday, date.tm_year + 1900);
-    
-
-    //     
-        
-    //     char *pushup_num;
-
-    //     pushup_num = strtok(first_line, ","); 
-        
-    //     //printf("\n%s", pushup_num);
-    //     while( pushup_num != NULL ) {
-    //       printf( " %s\n", pushup_num);
-        
-    //       pushup_num = strtok(NULL, first_line);
-    //    }
-
-
-
-        // pF = fopen(filename, "r");
-
-        // int current_line = 1;
-        // char c;
-        // do
-        // {
-        //     c = fgetc(pF);
-        //     if(c == '\n') current_line++;
-        // } while (c != EOF);
-
-        // printf("%d", current_line - 1);
-        // fclose(pF);
-        
-        /*while(keep_going)
-        {
-            fgets(buffer, BUFFER_SIZE, stdin);
-            
-        if (strcmp(buffer, "quit\n")== 0)
-        {
-            keep_going = false;
-        }
-        else fputs(buffer, pF);
-        }
-        fclose(pF);*/
     
     return 0;
 
